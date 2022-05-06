75db4779fe361ead2a5307be0f04df75bf55feef
tharodo <dophuongthao.459@gmail.com>
2022-05-07 00:24:01 +0700

    Using react hooks useEffect and useState

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Popular() {
+
+    const [popular, setPopular] = useState([]);
+    useEffect( () => {
+        getPopular();
+    }, [])
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+        const data = await api.json();
+        // console.log(data);
+        setPopular(data.recipes);
+    }
+
     return (
-        <div>Popular</div>
+        <div>
+            
+        </div>
     )
 }
 
